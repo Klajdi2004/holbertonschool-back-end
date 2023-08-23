afc5e529dacf9e4edf9c5b55b3170a287b272b01
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-08-23 12:55:36 +0200

    a

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,34 @@
+#!/usr/bin/python3
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress"""
 import requests
 import sys
 
-def fetch_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
 
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        employee_name = user_data['name']
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task['completed']]
-        num_done_tasks = len(done_tasks)
-
-        print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
-        for task in done_tasks:
-            print(f"\t{task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script_name.py employee_id")
+        print(f"UsageError: python3 {__file__} employee_id(int)")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_progress(employee_id)
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
+
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
+
+    employee_name = data[0]["user"]["name"]
+    total_tasks = len(data)
+    done_tasks = [task for task in data if task["completed"]]
+    total_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
