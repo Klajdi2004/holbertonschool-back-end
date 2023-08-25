deb59a21b4211c70f2658afcb6a50c657fd7d63f
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-08-25 14:47:27 -0700

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == "__main__":
-    """Get api bob"""
+    """Get API"""
     todos_api = requests.get(
         'https://jsonplaceholder.typicode.com/todos/')
     user_api = requests.get(
